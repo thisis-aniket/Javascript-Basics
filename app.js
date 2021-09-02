778ef343713c23f350c13af69d35e24e535b0988
unknown <you@example.com>
2021-09-02 19:29:08 +0530

    console basics

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello world')
+
+
+console.log({name: 'Aniket'});
+
+console.log(false, 23234343, 'string', {name : "Aniket"})
+console.warn("This is warning")
+
+console.error('this is error...')
