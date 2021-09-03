3f2c3b0d86735fd7bad6a45eab20bf15c4f74406
unknown <you@example.com>
2021-09-03 20:44:59 +0530

    Operators

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 //Operator
 // Arithmetic Operator
 
-let a = 12
-let b = 2
-
-console.log(a + b)
-
-console.log(a - b)
-
-console.log(a * b)
-
-console.log(a / b)
-
-console.log(a % b)
-
-console.log(a ** b)
+// let Name = "2"
+// let a = 2
 
 
+// a += Name
+// a/=7
+// a *=
+// a **=
+
+// Comparision Operator
+// const a = 21
+// const b = 7
+
+// console.log('a > b', a > b)
+// console.log('a == b', a == b)
+// console.log('a != b', a != b)
+// console.log('a >= b', a >= b)
+// console.log('a <= b', a <= b)
+
+// Logical Operators
+// console.log(true && false)
+// console.log(true || false)
+
+//Conditional Statements
+// function check(num) {
+//     if(num%2==0){
+//         console.log('even')
+//     }else if(num%2==1){
+//         console.log('odd')
+//     }else{
+//         console.log('zero')
+//     }
+// }
