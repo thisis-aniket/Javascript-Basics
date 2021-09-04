bd0f9b4c670c60b6d3d0f59372871695648f2059
unknown <you@example.com>
2021-09-04 19:57:52 +0530

    EventListeners

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const element = document.querySelector('#selectMe')
-// element.innerHTML = "This is from JS"
-
-// element.style.color = "cyan"
-// element.style.fontSize = "40px"
-// element.style = 'display:none'
-
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
-
+const button = document.querySelector('button')
 const body = document.querySelector('body')
 
-console.log(body.classList.toggle('.dark'))
-console.log(element.classList.contains('three'))
+const element = document.querySelector('#selectMe')
+
+button.addEventListener('click', () => {
+    body.classList.toggle("dark")
+})
+   
+
+    
