c1f4a490dcdc4cd9d778b62cd36d29f00015da6e
unknown <you@example.com>
2021-09-09 19:58:39 +0530

    Map and Filters

--- app.js
+++ app.js
@@ -35,25 +35,60 @@
 // };
 // console.log(add(1, 2));
 
-
 //Some more features
-const counter = {
-    count : 1,
-    next : function() {
-        return ++(this.count)
-    },
-    key: this.location,
-    prev : () => {
-        return --(this.count)
-    }
-}
+// const counter = {
+//     count : 1,
+//     next : function() {
+//         return ++(this.count)
+//     },
+//     key: this.location,
+//     prev : () => {
+//         return --(this.count)
+//     }
+// }
 
-console.log(counter)
-console.log(counter.next())
-console.log(counter)
-console.log(counter.next())
-console.log(counter)
+// console.log(counter)
+// console.log(counter.next())
+// console.log(counter)
+// console.log(counter.next())
+// console.log(counter)
 // Output will NaN arrow function will not this keyword but just this keyword return window
-console.log(counter.prev())
+// console.log(counter.prev())
 
 // console.log(counter.key)
+
+// Map and Filter ES6
+// Map function is same as the forEach but forEach don't return anything
+let marks = [1, 2, 3, 4, 5, 6];
+// const newArr = marks.map((e, index) => {
+//   return index;
+// });
+// console.log(newArr);
+
+
+// console.log(
+//   ranks.map((e, index) => {
+//     return e * 100;
+//   })
+// );
+
+// console.log(
+//   ranks.forEach((e, index) => {
+//     return e * 100;
+//   })
+// );
+
+// let newarr = ranks.map((e, index) => {
+//     if(e%2===0)
+//         return e
+    
+// })
+// console.log(newarr);
+
+// Filter
+let newfilterarr = marks.filter((e, index) => {
+    console.log(e, index)
+    if(e%2===0)
+        return e
+})
+console.log(newfilterarr)
